001daf383748e185f3cdd4ad9d431154e90435a6
Minh Dao T <minh.dao@asiantech.vn>
2018-01-03 11:05:42 +0700

    add comment for function index

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -7,6 +7,12 @@ use App\Http\Controllers\Controller;
 
 class BookController extends Controller
 {
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         return view('backend.books.index');
