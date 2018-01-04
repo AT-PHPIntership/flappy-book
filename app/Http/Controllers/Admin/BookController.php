aba794fc41deed63d807d5dd5f7198eafea7f13a
at-longdau <longdau@asiantech.vn>
2018-01-04 16:22:30 +0700

    update validate edit book

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\EditBookRequest;
 
 class BookController extends Controller
 {
@@ -16,5 +17,25 @@ class BookController extends Controller
     public function index()
     {
         return view('backend.books.index');
+    }
+    /**
+     * Show form edit.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function edit()
+    {
+        return view('backend.books.edit');
+    }
+    /**
+     * Request form edit.
+     *
+     * @param App\Http\Requests\EditBookRequest $request create
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(EditBookRequest $request)
+    {
+        dd($request);
     }
 }
---
+++ app/Http/Requests/EditBookRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EditBookRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|min:2',
+            'price' => 'required|numberic',
+            'from_person' => 'required',
+            'description' => 'required|string',
+            'year' => 'required','numberic',
+            'author' => 'required',
+            'image' => 'image|max:10240',
+        ];
+    }
+}
