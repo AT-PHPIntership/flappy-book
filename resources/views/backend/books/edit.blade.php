52af99b93c9d53fbc114e7e57dba7e561d171372
at-longdau <longdau@asiantech.vn>
2018-01-04 16:06:14 +0700

    validate edit book

--- resources/views/backend/books/edit.blade.php
+++ resources/views/backend/books/edit.blade.php
@@ -43,33 +43,54 @@
                       <div class="form-group">
                         <label for="InputTitle">{{ __('Title') }}</label>
                         <input type="text" id="InputTitle" class="form-control" name="title" placeholder="{{ __('Title') }}">
+                  @if($errors->first('title'))
+                   <span class="text-danger">{{ $errors->first('title') }}</span>
+                  @endif
                       </div>
                       <div class="form-group">
                         <label for="InputPrice">{{ __('Price') }}</label>
-                        <input type="text" id="InputPrice" class="form-control" name="price" placeholder="{{ __('Price') }}">  
+                        <input type="text" id="InputPrice" class="form-control" name="price" placeholder="{{ __('Price') }}">
+                  @if($errors->first('price'))
+                   <span class="text-danger">{{ $errors->first('price') }}</span>
+                  @endif      
                       </div>
                       <div class="form-group">
-                        <label for="InputFromPerson">{{ __('From Person') }}</label>
-                        <input type="text" id="InputFromPerson" class="form-control" name="from_person" placeholder="{{ __('ID Donator') }}">   
+                        <label for="InputIDFromPerson">{{ __('From Person') }}</label>
+                        <input type="text" id="InputFromPerson" class="form-control" name="from_person" placeholder="{{ __('ID Donator') }}">
+                  @if($errors->first('from_person'))
+                   <span class="text-danger">{{ $errors->first('from_person') }}</span>
+                  @endif      
                       </div>
                       <div class="form-group">
                         <label for="InputDescription">{{ __('Description') }}</label>
-                        <textarea class="textarea" id="InputDescription" placeholder="Place some text here"
+                        <textarea class="textarea" id="InputDescription" class="form-gr placeholder="Place some text here"
                           style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
+                  @if($errors->first('description'))
+                   <span class="text-danger">{{ $errors->first('description') }}</span>
+                  @endif
                       </div>
                       <div class="form-group">
-                        <label for="InputYear">{{ __('Year') }}</label>
-                        <input type="number" id="InputYear" class="form-control" name="year" placeholder="">
+                        <label for="exampleInputYear">{{ __('Year') }}</label>
+                        <input type="number" class="form-control" name="year" placeholder="">
+                  @if($errors->first('year'))
+                   <span class="text-danger">{{ $errors->first('year') }}</span>
+                  @endif
                       </div>
                       <div class="form-group">
-                        <label for="InputAuthor">{{ __('Author') }}</label>
-                        <input type="text" id="InputAuthor" class="form-control" name="author" placeholder="{{ __('Author') }}">    
+                        <label for="exampleInputAuthor">{{ __('Author') }}</label>
+                        <input type="text" class="form-control" name="author" placeholder="{{ __('Author') }}">
+                  @if($errors->first('author'))
+                   <span class="text-danger">{{ $errors->first('author') }}</span>
+                  @endif      
                       </div>
                     </div>
                   </div>
                   <div class="form-group">
-                    <label for="InputFile">{{ __('Picture') }}</label>
-                    <input type="file" name="picture" id="InputFile">
+                    <label for="exampleInputFile">{{ __('Picture') }}</label>
+                    <input type="file" name="picture">
+                  @if($errors->first('picture'))
+                   <span class="text-danger">{{ $errors->first('picture') }}</span>
+                  @endif
                   </div>
               </div>
               <div class="box-footer">
