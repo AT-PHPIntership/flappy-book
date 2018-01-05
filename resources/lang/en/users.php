4926ed7bff0bcc5e3961bf5b17b5adbc26a43dea
Minh Dao T <minh.dao@asiantech.vn>
2018-01-05 11:17:36 +0700

    multi language for users

---
+++ resources/lang/en/users.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'list_users' => 'List Users',  
+    'users' => 'Users',
+    'home' => 'Home',
+    'list' => 'List',
+    'no' => 'No',
+    'employee_code' => 'Employee Code',
+    'name' => 'Name',
+    'email' => 'Email',
+    'donate' => 'Donate',
+    'borrowed' => 'Borrowed',
+    'detail_user' => 'Detail User',
+    'user_profile' => 'User Profile',
+    'role' => 'Role',
+    'books_donated' => 'Books Donated',
+    'books_borrowed' => 'Books Borrowed',
+    'books_borrowing' => 'Books Borrowing',
+    'back' => 'Back',
+];
