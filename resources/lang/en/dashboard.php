c1ff7b3d0b18d1104856f77fc24f61cbe8bb3ea6
Minh Dao T <minh.dao@asiantech.vn>
2018-01-05 10:36:31 +0700

    fix content layout master & using multi language

---
+++ resources/lang/en/dashboard.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'online' => 'Online',    
+    'main_navigation' => 'MAIN NAVIGATION',
+    'dashboard' => 'Dashboard',
+    'books' => 'Books',
+    'users' => 'Users',
+    'home' => 'Home',
+    'more_info' => 'More Info',
+];
