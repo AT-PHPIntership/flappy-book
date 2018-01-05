f7442a7e6b740a15ec1612d985e815ee533a9962
at-hieule <hieu.le@asiantech.vn>
2018-01-05 16:07:05 +0700

    fix path models

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,17 +6,20 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
+    protected $table = 'users';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-         'employee_code',
+         'employ_code',
          'name',
          'email',
          'team',
          'avatar_url',
+         'is_admin',
          'access_token',
          'expires_at',
      ];
