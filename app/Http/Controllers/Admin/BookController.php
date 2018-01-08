de4a6d8a6ece85ee2249df9d302166cada682938
at-longdau <long.dau@asiantech.vn>
2018-01-08 17:45:36 +0700

    update validate edit book

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\EditBookRequest;
+use App\Http\Requests\Backend\EditBookRequest;
 use Illuminate\Support\Facades\DB;
 use App\Model\Book;
 use App\Model\Category;
