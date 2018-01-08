016ad31bd15ef5af3528b3c4a0b8346527ba0633
Minh Dao T <minh.dao@asiantech.vn>
2018-01-08 15:34:37 +0700

    get data list books

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -4,18 +4,31 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use App\Model\Book;
 
 class BookController extends Controller
 {
 
     /**
-     * Display a listing of the resource.
+     * Display a listing of the books.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        return view('backend.books.index');
+        $fields = [
+            'books.id',
+            'books.title',
+            'books.author',
+            'books.rating',
+            DB::raw('COUNT(borrows.id) AS total_borrowed'),
+        ];
+        $books = Book::leftJoin('borrows', 'books.id', '=', 'borrows.book_id')
+                     ->select($fields)
+                     ->groupBy('books.id')
+                     ->paginate(config('define.row_count'));
+        return view('backend.books.index', compact('books'));
     }
 
     /**
