d1be7e43d37e496e454a68bda34d44b1a63a6943
Tram <tram.pham@asiantech.vn>
2018-01-09 18:42:31 +0700

    fix use DB

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\DB;
+use DB;
 use App\Model\User;
 
 class UserController extends Controller
