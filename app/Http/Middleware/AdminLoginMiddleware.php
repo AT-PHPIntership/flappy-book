8d54f8f146fe65a892765ed3a56837c73e3c51ec
at-hieule <hieu.le@asiantech.vn>
2018-01-09 16:00:46 +0700

    Create middleware for admin login

---
+++ app/Http/Middleware/AdminLoginMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use app\Model\User;
+
+class AdminLoginMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request request
+     * @param \Closure                 $next    next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::Check()) {
+            $user = Auth::user();
+            if ($user->is_admin == User::ROLE_ADMIN) {
+                return $next($request);
+            }
+            return redirect('/login');
+        } else {
+            return redirect('/login');
+        }
+    }
+}
