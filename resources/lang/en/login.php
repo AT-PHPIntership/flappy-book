c12e0c454c3abddc89d3fec892caf29177136c6c
at-hieule <hieu.le@asiantech.vn>
2018-01-11 06:32:50 +0700

    Fix multi language for login form

---
+++ resources/lang/en/login.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'title' =>'LIBRARY| Log in',
+    'start_session' => 'Sign in to start your session',
+    'email' => 'Email',
+    'password' => 'Password',
+    'remember_me' => 'Remember Me',
+    'sign_in' => 'Sign In',
+    'forgot_password' => 'I forgot my password',
+];
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>{{ __('LIBRARY| Log in') }}</title>
+  <title>{{ __('login.title') }}</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -34,33 +34,33 @@
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
-    <p class="login-box-msg">{{ __('Sign in to start your session')}}</p>
+    <p class="login-box-msg">{{ __('login.start_session')}}</p>
     <form action="{{ route('login') }}" method="POST">
       {{ csrf_field() }}
       <div class="form-group has-feedback">
-        <input type="email" name="email" class="form-control" placeholder="{{ __('Email')}}">
+        <input type="email" name="email" class="form-control" placeholder="{{ __('login.email')}}">
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" name="password" class="form-control" placeholder="{{ __('Password')}}">
+        <input type="password" name="password" class="form-control" placeholder="{{ __('login.password')}}">
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
       <div class="row">
         <div class="col-xs-8">
           <div class="checkbox icheck">
             <label>
-              <input type="checkbox"> {{ __('Remember Me')}} 
+              <input type="checkbox"> {{ __('login.remember_me')}} 
             </label>
           </div>
         </div>
         <!-- /.col -->
         <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">{{ __('Sign In')}}</button>
+          <button type="submit" class="btn btn-primary btn-block btn-flat">{{ __('login.sign_in')}}</button>
         </div>
         <!-- /.col -->
       </div>
     </form>
-    <a href="{{ asset('http://portal.asiantech.vn/auth/login') }}">{{ __('I forgot my password')}}</a><br>
+    <a href="{{ asset('http://portal.asiantech.vn/auth/login') }}">{{ __('login.forgot_password')}}</a><br>
   </div>
   <!-- /.login-box-body -->
 </div>
