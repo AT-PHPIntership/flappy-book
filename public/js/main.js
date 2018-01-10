d70a8334d10eaba679bd9aaa5a4ecb296e4c350d
Cao Nguyen V <cao.nguyen@asiantech.vn>
2018-01-10 09:34:26 +0700

    fix convention

--- public/js/main.js
+++ public/js/main.js
@@ -1,38 +1,38 @@
 $(document).ready(function(){
-  $('[data-toggle="tooltip"]').tooltip();
-  
-  /**
-   * Show delete confimation when click button delete
-   */
-  $('.btn-delete-item').bind('click',function(e){
-      e.preventDefault();
-      var form = $(this.form);
-      var title = $(this).attr('data-title');
-      var body = '<i>' + $(this).attr('data-confirm') + '</i>';
-      $('#title-content').html(title);
-      $('#body-content').html(body);
-      $('#confirm').modal('show');
-      $('#delete-btn').one('click', function(){
-          form.submit();
-      })
-  });
+    $('[data-toggle="tooltip"]').tooltip();
+
+    /**
+    * Show delete confimation when click button delete
+    */
+    $('.btn-delete-item').bind('click',function(e){
+        e.preventDefault();
+        var form = $(this.form);
+        var title = $(this).attr('data-title');
+        var body = '<i>' + $(this).attr('data-confirm') + '</i>';
+        $('#title-content').html(title);
+        $('#body-content').html(body);
+        $('#confirm').modal('show');
+        $('#delete-btn').one('click', function(){
+            form.submit();
+        })
+    });
 });
 
 $(document).ready(function(){
-  let url = new URL(document.location);
-  let params = url.searchParams;
-  let order = params.get('order');
-  let filter = params.get('filter');
+    let url = new URL(document.location);
+    let params = url.searchParams;
+    let order = params.get('order');
+    let filter = params.get('filter');
 
-  $('.sort-element').each(function(){
-    if (filter == $(this).attr('name')) {
-      if (order == 'desc') {
-        $(this).children().attr('class', 'fa fa-sort-amount-desc');
-      } else {
-        $(this).children().attr('class', 'fa fa-sort-amount-asc');
-        params.set('order', 'desc');
-        $(this).attr('href', url);
-      }
-    }
-  });
+    $('.sort-element').each(function(){
+        if (filter == $(this).attr('name')) {
+            if (order == 'desc') {
+                $(this).children().attr('class', 'fa fa-sort-amount-desc');
+            } else {
+                $(this).children().attr('class', 'fa fa-sort-amount-asc');
+                params.set('order', 'desc');
+                $(this).attr('href', url);
+            }
+        }
+    });
 });
