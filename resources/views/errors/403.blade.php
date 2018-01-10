e35e6d708c9e191954d8b3e4578fff2f6724a1e8
at-hieule <hieu.le@asiantech.vn>
2018-01-10 10:32:29 +0700

    add link go back

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -3,9 +3,14 @@
 <head>
   <title>{{ __('403 Forbidden ')}}</title>
   <meta http-equiv="refresh" content="5;url={{ route('home') }}"/>
+  <link rel="stylesheet" href="{{ asset('bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
 </head>
 <body>
-  <h2>{{ __('Forbidden') }}</h2>
-  <p>{{ $exception->getMessage() }}</p>
+  <div class="col-md-8">
+    <h1>{{ __('Forbidden') }}</h1>
+    <h2 class="text-danger">{{ $exception->getMessage() }}</h2>
+    <h3><a  class="col-sm-3" href="javascript:history.back()" title="">{{ __('Go back') }}</a></h3>
+    <h3><a href="{{ route('home') }}" title="">{{ __('Go to home page') }}</a></h3>
+  </div>
 </body>
 </html>
