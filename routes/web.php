98020b04380d11c137146798cbd0ec543b08eece
at-hieule <hieu.le@asiantech.vn>
2018-01-10 08:08:52 +0700

    fix space at middleware line

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['namespace'=>'Admin', 'prefix'=>'admin' , 'middleware'=>'admin'], function(){
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin' , 'middleware' => 'admin'], function(){
     Route::get('/', function () {
         return view('backend.home.index');
     });
