dd186935614f8dd5c4d1bc93dbfc1d2df220b804
at-hieule <hieu.le@asiantech.vn>
2018-01-10 09:35:53 +0700

    Fix multi language

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>403 Forbidden</title>
-  <meta http-equiv="refresh" content="5;url={{ route('/') }}"/>
+  <title>{{ __('403 Forbidden ')}}</title>
+  <meta http-equiv="refresh" content="5;url={{ route('home') }}"/>
 </head>
 <body>
-  <h2>Forbidden</h2>
+  <h2>{{ __('Forbidden') }}</h2>
   <p>{{ $exception->getMessage() }}</p>
 </body>
 </html>
