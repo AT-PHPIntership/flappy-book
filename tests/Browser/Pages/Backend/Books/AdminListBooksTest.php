80b57d64ef7b186192540e01d2b84478e401fae1
Minh Dao T <minh.dao@asiantech.vn>
2018-01-10 17:10:22 +0700

    CI with mysql

--- tests/Browser/Pages/Backend/Books/AdminListBooksTest.php
+++ tests/Browser/Pages/Backend/Books/AdminListBooksTest.php
@@ -25,87 +25,87 @@ class AdminListBooksTest extends DuskTestCase
         $this->makeUserLogin();
         $this->browse(function (Browser $browser) {
             $browser->loginAs(User::find(1))
-                    ->visit('/')
-                    // ->clickLink('Books')
-                    // ->assertPathIs('/admin/books')
-                    ->assertSee('Laravel');
+                    ->visit('/admin')
+                    ->clickLink('Books')
+                    ->assertPathIs('/admin/books')
+                    ->assertSee('List Books');
         });
     }
 
-    // /**
-    //  * Test view Admin List Books if database empty
-    //  *
-    //  * @return void
-    //  */
-    // public function testListBooksEmpty()
-    // {
-    //     $this->makeUserLogin();
-    //     $this->browse(function (Browser $browser) {
-    //         $browser->loginAs(User::find(1))
-    //                 ->visit('/admin/books')
-    //                 ->assertSee('List Books');
-    //         $elements = $browser->elements('#list-books tbody tr');
-    //         $this->assertCount(0, $elements);
-    //         $this->assertNull($browser->element('.paginate'));
-    //     });
-    // }
+    /**
+     * Test view Admin List Books if database empty
+     *
+     * @return void
+     */
+    public function testListBooksEmpty()
+    {
+        $this->makeUserLogin();
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::find(1))
+                    ->visit('/admin/books')
+                    ->assertSee('List Books');
+            $elements = $browser->elements('#list-books tbody tr');
+            $this->assertCount(0, $elements);
+            $this->assertNull($browser->element('.paginate'));
+        });
+    }
     
-    // /**
-    //  * Test view Admin List Books if database has record
-    //  *
-    //  * @return void
-    //  */
-    // public function testListBooksHasRecord()
-    // {
-    //     $this->makeUserLogin();
-    //     $this->makeData(2);
-    //     $this->browse(function (Browser $browser) {
-    //         $browser->loginAs(User::find(1))
-    //                 ->visit('/admin/books')
-    //                 ->assertSee('List Books');
-    //         $elements = $browser->elements('#list-books tbody tr');
-    //         $this->assertCount(2, $elements);
-    //         $this->assertNull($browser->element('.pagination'));
-    //     });
-    // }
+    /**
+     * Test view Admin List Books if database has record
+     *
+     * @return void
+     */
+    public function testListBooksHasRecord()
+    {
+        $this->makeUserLogin();
+        $this->makeData(2);
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::find(1))
+                    ->visit('/admin/books')
+                    ->assertSee('List Books');
+            $elements = $browser->elements('#list-books tbody tr');
+            $this->assertCount(2, $elements);
+            $this->assertNull($browser->element('.pagination'));
+        });
+    }
 
-    // /**
-    //  * Test view Admin List Books with pagination
-    //  *
-    //  * @return void
-    //  */
-    // public function testListBooksPagination()
-    // {
-    //     $this->makeUserLogin();
-    //     $this->makeData(12);
-    //     $this->browse(function (Browser $browser) {
-    //         $browser->loginAs(User::find(1))
-    //                 ->visit('/admin/books')
-    //                 ->assertSee('List Books');
-    //         // Count row number in one page
-    //         $elements = $browser->elements('#list-books tbody tr');
-    //         $this->assertCount(10, $elements);
-    //         $this->assertNotNull($browser->element('.pagination'));
-    //         //Count page number of pagination
-    //         $paginate_element = $browser->elements('.pagination li');
-    //         $number_page = count($paginate_element)- 2;
-    //         $this->assertTrue($number_page == 2);
-    //     });
-    // }
+    /**
+     * Test view Admin List Books with pagination
+     *
+     * @return void
+     */
+    public function testListBooksPagination()
+    {
+        $this->makeUserLogin();
+        $this->makeData(12);
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::find(1))
+                    ->visit('/admin/books')
+                    ->assertSee('List Books');
+            // Count row number in one page
+            $elements = $browser->elements('#list-books tbody tr');
+            $this->assertCount(10, $elements);
+            $this->assertNotNull($browser->element('.pagination'));
+            //Count page number of pagination
+            $paginate_element = $browser->elements('.pagination li');
+            $number_page = count($paginate_element)- 2;
+            $this->assertTrue($number_page == 2);
+        });
+    }
 
-    // public function testPathPagination()
-    // {
-    //     $this->makeUserLogin();
-    //     $this->makeData(12);
-    //     $this->browse(function (Browser $browser) {
-    //         $browser->loginAs(User::find(1))
-    //                 ->visit('/admin/books?page=2')
-    //                 ->assertSee('List Books');
-    //         $elements = $browser->elements('#list-books tbody tr');
-    //         $this->assertCount(2, $elements);
-    //         $browser->assertQueryStringHas('page', 2);
-    //     });
-    // }
+    public function testPathPagination()
+    {
+        $this->makeUserLogin();
+        $this->makeData(12);
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::find(1))
+                    ->visit('/admin/books?page=2')
+                    ->assertSee('List Books');
+            $elements = $browser->elements('#list-books tbody tr');
+            $this->assertCount(2, $elements);
+            $browser->assertQueryStringHas('page', 2);
+        });
+    }
 
     /**
      * Make user belong team PHP and is admin
