e2cf4490d4888e9292b4dc8778e686dc40c6b52c
Cao Nguyen V <cao.nguyen@asiantech.vn>
2018-01-16 17:20:13 +0700

    add qrcode model

---
+++ app/Model/Qrcode.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Qrcode extends Model
+{
+    /**
+     * Declare table
+     *
+     * @var string $tabel table name
+     */
+    protected $table = 'qrcodes';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id',
+        'book_id',
+        'prefix',
+        'code_id',
+        'status'
+    ];
+
+    /**
+     * Relationship belongsTo with Book
+     *
+     * @return array
+     */
+    public function books()
+    {
+        return $this->belongsTo(Book::class, 'book_id');
+    }
+}
