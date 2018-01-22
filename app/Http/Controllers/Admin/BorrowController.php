c917e512310c34703f17edf0207dcbde887004e2
Tram <tram.pham@asiantech.vn>
2018-01-22 13:10:47 +0700

    fix circle CI

--- app/Http/Controllers/Admin/BorrowController.php
+++ app/Http/Controllers/Admin/BorrowController.php
@@ -14,7 +14,6 @@ class BorrowController extends Controller
      */
     public function index()
     {
-        return view ('backend.borrows.index');
+        return view('backend.borrows.index');
     }
-
 }
