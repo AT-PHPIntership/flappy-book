c00a08c4ffbaff7050596edc6b5f8d0be19f6b56
at-duongtruong <duong.truong@asiantech.vn>
2018-01-24 14:04:20 +0700

    fix comment

--- resources/lang/en/borrows.php
+++ resources/lang/en/borrows.php
@@ -13,6 +13,6 @@ return [
     'search' => 'Search',
     'list_search' =>[
         \App\Model\Borrow::TYPE_NAME => 'Name',
-        \App\Model\Borrow::TYPE_BORROWS => 'Book Borrowing',
+        \App\Model\Borrow::TYPE_BORROWING => 'Book Borrowing',
     ],
 ];
