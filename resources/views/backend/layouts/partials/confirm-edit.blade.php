410f0bb1dbd403abea9a6f9880a364820629d3f6
Cao Nguyen V <cao.nguyen@asiantech.vn>
2018-01-25 09:55:26 +0700

    fix layout, witdh of buttons

--- resources/views/backend/layouts/partials/confirm-edit.blade.php
+++ resources/views/backend/layouts/partials/confirm-edit.blade.php
@@ -6,7 +6,7 @@
             <h4 class="modal-title" id="title-content">{{ __('categories.confirm_edit') }}</h4>
         </div>
         <div class="modal-body" id="body-content"></div>
-        <div class="modal-footer">
+        <div class="modal-footer popup-edit-categories">
             <button type="button" class="btn btn-sm btn-flat btn-primary" id="edit-btn" data-dismiss="modal">{{ __('categories.edit') }}</button>
             <button type="button" class="btn btn-sm btn-flat btn-warning" id="reset-btn" data-dismiss="modal">{{ __('categories.reset') }}</button>
             <button type="button" class="btn btn-sm btn-flat btn-default" id="cancel-btn" data-dismiss="modal">{{ __('categories.cancel') }}</button>
