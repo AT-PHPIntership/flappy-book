b08377466cb7a9ee75d3b83c3245cc5f19d63a40
Minh Dao T <minh.dao@asiantech.vn>
2018-01-31 11:41:59 +0700

    create seeder for table ratings

---
+++ database/seeds/RatingsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Database\Eloquent\Model;
+
+class RatingsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $postId = DB::table('posts')->where('status', 2)->pluck('id')->toArray();
+        $faker = Faker::create();
+        for ($i = 0; $i <= 15; $i++) {
+            factory(App\Model\Rating::class)->create([
+                'post_id' => $faker->randomElement($postId),
+            ]);
+        }
+    }
+}
