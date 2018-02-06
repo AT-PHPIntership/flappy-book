a4509c2df17e0f1ea6b936251b2a33d308c2652b
Tram <tram.pham@asiantech.vn>
2018-02-06 14:18:26 +0700

    layout qrcode not print yet list

---
+++ resources/lang/en/qrcodes.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'list_qrcodes' => 'List Qrcodes',
+    'home' => 'Home',
+    'qrcodes' => 'Qrcode',
+    'list' => 'List',
+    'no' => 'No',
+    'download' => 'Download',
+    'qrcode' => 'Qrcode',
+];
