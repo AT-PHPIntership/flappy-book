640d9e53c3667cc095f014ad3f3900d33b1a8739
at-longdau <long.dau@asiantech.vn>
2018-02-06 11:26:10 +0700

    update delete comment

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,17 +1,17 @@
-    var $id = '';
-    $('.confirm-delete').on('click',function(e){
-        e.preventDefault();
-        $id = $(this).data('id');
-    })
-    $('#delete-btn').click(function(){
-   		token = $('meta[name="csrf_token"]').attr('content');
+var $id = '';
+$('.confirm-delete').on('click',function(e){
+    e.preventDefault();
+    $id = $(this).data('id');
+})
+$('#delete-btn').click(function(){
+	token = $('meta[name="csrf_token"]').attr('content');
     	
-    	$.ajax({
-    		url : '/admin/comments/'+ $id,
-    		type : 'DELETE',
-    		headers: { 'X-CSRF-TOKEN': token },
-    		success: function(data){
-    		},
-    	});
-    	$('#confirm').modal('hide');
-    })
+	$.ajax({
+		url : '/admin/comments/'+ $id,
+		type : 'DELETE',
+		headers: { 'X-CSRF-TOKEN': token },
+		success: function(data){
+		},
+	});
+	$('#confirm').modal('hide');
+})
--- resources/lang/en/common.php
+++ resources/lang/en/common.php
@@ -5,8 +5,4 @@ return [
         'delete_post' => 'Do you want to delete this post',
         'delete_comment' => 'Do you want to delete this comment',
     ],
-    'comment' => [
-        'delete_success' => 'Delete comment success!',
-        'delete_failed' => 'Delete comment failed!',
-    ],
 ];
