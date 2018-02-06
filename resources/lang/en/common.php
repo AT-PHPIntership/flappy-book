64bd67288a04e7f97d7bce35def66cc678206622
at-longdau <long.dau@asiantech.vn>
2018-02-06 11:28:20 +0700

    update delete comment

--- resources/lang/en/common.php
+++ resources/lang/en/common.php
@@ -3,6 +3,6 @@ return [
     'confirm' => [
         'title' => 'Confirm Delete !',
         'delete_post' => 'Do you want to delete this post',
-        'delete_comment' => 'Do you want to delete this comment',
+        'delete_comment' => 'Do you want to delete this comment'
     ],
 ];
