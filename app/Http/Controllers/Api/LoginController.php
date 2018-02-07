b8c7ff820898a94b1c51936cc308a011e8c57560
Le Thi Hieu <hieu.le@asiantech.vn>
2018-02-07 14:37:41 +0700

    Login system API

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Controllers\Controller;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ServerException;
+use App\Model\User;
+use DB;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle a login request to the application.
+     *
+     * @param \Illuminate\Http\Request $request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $data = $request->except('_token');
+        try {
+            $client = new Client();
+            $portal = $client->post(config('portal.base_url_api') . config('portal.end_point.login'), ['form_params' => $data]);
+            $portalResponse = json_decode($portal->getBody()->getContents());
+            if ($portalResponse->meta->status == config('define.login.msg_success')) {
+                $user = $this->saveUser($portalResponse, $request);
+                return  response()->json([
+                    'meta' => $portalResponse->meta,
+                    'data' => array_except($user->toArray(), 'deleted_at')
+                ], Response::HTTP_OK);
+            } else {
+                return response()->json(['message' => trans('portal.messages.server_error')], Response::HTTP_INTERNAL_SERVER_ERROR);
+            }
+        } catch (ServerException $e) {
+            $portalResponse = json_decode($e->getResponse()->getBody()->getContents());
+            return response()->json($portalResponse, Response::HTTP_BAD_REQUEST);
+        }
+    }
+
+    /**
+     * Save data users
+     *
+     * @param App\Http\Controllers\Auth $portalResponse $portalResponse
+     * @param \Illuminate\Http\Request  $request        $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     */
+    public function saveUser($portalResponse, $request)
+    {
+        $userResponse = $portalResponse->data->user;
+        # Collect user data from response
+        $userCondition = [
+            'employ_code' => $userResponse->employee_code,
+            'email' => $request->email,
+        ];
+        $user = [
+            'name' => $userResponse->name,
+            'team' => $userResponse->teams[0]->name,
+            'expires_at' => date(config('define.login.datetime_format'), strtotime($userResponse->expires_at)),
+            'avatar_url' => $userResponse->avatar_url,
+            'access_token' => $userResponse->access_token,
+        ];
+        if ($userResponse->teams[0]->name == User::TEAM_SA) {
+            $user['is_admin'] = User::ROLE_ADMIN;
+        }
+        # Get user from database OR create User
+        return User::updateOrCreate($userCondition, $user);
+    }
+}
