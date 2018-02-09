a52a06d7f2faf120fe5e17d381ab9cd1dd6d725f
Le Thi Hieu <hieu.le@asiantech.vn>
2018-02-09 16:14:46 +0700

    Add apiController

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Libraries\Traits\ApiResponse;
+use App\Http\Controllers\Controller;
+
+
+class ApiController extends Controller
+{
+    use ApiResponse;   
+}
