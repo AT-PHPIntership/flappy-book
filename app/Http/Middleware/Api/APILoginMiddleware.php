ca208c36bcdcd7c9885230e642a0df77439e19f9
Le Thi Hieu <hieu.le@asiantech.vn>
2018-02-22 15:34:02 +0700

    Middleware Login system

---
+++ app/Http/Middleware/Api/APILoginMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware\Api;
+use App\Model\User;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
+
+use Closure;
+
+class APILoginMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $accessToken = $request->headers->get('token');
+        $user = User::where('access_token', '=', $accessToken)
+                    ->where('access_token', '<>', null)
+                    ->firstOrFail();
+        if (isset($user) && Carbon::parse($user->expires_at)->diffInSeconds(Carbon::now()) > 0) {
+            Auth::login($user);
+            return $next($request);
+        }
+    }
+}
