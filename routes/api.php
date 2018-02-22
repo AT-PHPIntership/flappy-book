8384906166b64d0e4e7e80a2eab24e536027e1bf
Minh Dao T <minh.dao@asiantech.vn>
2018-02-22 08:56:49 +0700

    add namespace for route

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('books/{book}', 'Api\BookController@show');
+Route::group(['namespace' => 'Api'], function(){
+    Route::get('books/{book}', 'BookController@show');
+});
