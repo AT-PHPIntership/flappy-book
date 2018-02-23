1125bfd311d9872256a14b99d014676b3d24a6fc
nguyennhan <nhan.nguyen2@asiantech.vn>
2018-02-23 08:52:34 +0700

    delete query database

--- tests/Browser/Pages/Backend/Posts/DetailPostTest.php
+++ tests/Browser/Pages/Backend/Posts/DetailPostTest.php
@@ -19,6 +19,9 @@ class DetailPostTest extends DuskTestCase
 {
     use DatabaseMigrations;
 
+    const NUMBER_RECORD_LIKE = 5;
+    const RATING = 5;
+
     /**
      * Override function setUp()
      *
@@ -27,8 +30,6 @@ class DetailPostTest extends DuskTestCase
     public function setUp()
     {
        parent::setUp();
-
-       $this->makeData();
     }
 
     /**
@@ -38,17 +39,17 @@ class DetailPostTest extends DuskTestCase
      */
     public function testDetailPostStatus()
     {
+        $this->makeData(0);
         $this->browse(function (Browser $browser) {
-            $post = $this->makeQueryDetailPost(1);
             $browser->loginAs($this->user)
                 ->resize(1920, 1080)
                 ->visit('/admin/posts/1')
-                ->assertSee('Detail post')
+                ->assertSee('Detail Post')
                 ->assertSee('List comments')
                 ->assertDontSee('Rating');
 
                 $likes = $browser->text('ul.list-group-unbordered li:nth-child(2) a');
-                $this->assertEquals($post->likes, $likes);
+                $this->assertEquals(self::NUMBER_RECORD_LIKE, $likes);
         });
     }
 
@@ -59,18 +60,18 @@ class DetailPostTest extends DuskTestCase
      */
     public function testDetailPostFindBook()
     {
+        $this->makeData(1);
         $this->browse(function (Browser $browser) {
-            $post = $this->makeQueryDetailPost(2);
             $browser->loginAs($this->user)
                 ->resize(1920, 1080)
-                ->visit('/admin/posts/2')
+                ->visit('/admin/posts/1')
                 ->assertSee('Find book')
-                ->assertSee('Detail post')
+                ->assertSee('Detail Post')
                 ->assertSee('List comments')
                 ->assertDontSee('Rating');
 
                 $likes = $browser->text('ul.list-group-unbordered li:nth-child(2) a');
-                $this->assertEquals($post->likes, $likes);
+                $this->assertEquals(self::NUMBER_RECORD_LIKE, $likes);
         });
     }
 
@@ -81,21 +82,21 @@ class DetailPostTest extends DuskTestCase
      */
     public function testDetailPostReviewBook()
     {
+        $this->makeData(2);
         $this->browse(function (Browser $browser) {
-            $post = $this->makeQueryDetailPost(3);
             $browser->loginAs($this->user)
                 ->resize(1920, 1080)
-                ->visit('/admin/posts/3')
+                ->visit('/admin/posts/1')
                 ->assertSee('Review')
-                ->assertSee('Detail post')
+                ->assertSee('Detail Post')
                 ->assertSee('List comments')
                 ->assertSee('Book')
                 ->assertSee('Rating');
 
             $rating = $browser->text('ul.list-group-unbordered li:nth-child(3) a');
             $likes = $browser->text('ul.list-group-unbordered li:nth-child(4) a');
-            $this->assertEquals($post->rating, $rating);
-            $this->assertEquals($post->likes, $likes);
+            $this->assertEquals(self::RATING, $rating);
+            $this->assertEquals(self::NUMBER_RECORD_LIKE, $likes);
         });
     }
 
@@ -104,7 +105,7 @@ class DetailPostTest extends DuskTestCase
      *
      * @return void
      */
-    public function makeData()
+    public function makeData($status)
     {
         $faker = Faker::create();
 
@@ -121,53 +122,32 @@ class DetailPostTest extends DuskTestCase
 
         // Create posts
         $userId = $this->user->id;
-        for ($i = 0; $i < 3; $i++) {
-            factory(Post::class)->create([
-                'status'  => $i,
-                'user_id' => $userId,
-            ]);
-        }
+        factory(Post::class)->create([
+            'status'  => $status,
+            'user_id' => $userId,
+        ]);
+
+        // Create likes
+        factory(Like::class, self::NUMBER_RECORD_LIKE)->create([
+            'post_id' => 1,
+            'user_id' => $userId,
+        ]);
 
         // Create comments
-        $postId = DB::table('posts')->pluck('id')->toArray();
         for ($i = 0; $i < 15; $i++) {
             factory(Comment::class)->create([
                 'user_id' => $userId,
                 'commentable_type' => 'post',
-                'commentable_id' => $faker->randomElement($postId),
-            ]);
-        }
-
-        // Create likes
-        for ($i = 0; $i < 15; $i++) {
-            factory(Like::class)->create([
-                'post_id' => $faker->randomElement($postId),
-                'user_id' => $userId,
+                'commentable_id' => 1,
             ]);
         }
 
         // Create rating
         $bookId = DB::table('books')->pluck('id')->toArray();
         factory(Rating::class)->create([
-            'post_id' => '3',
+            'rating' => self::RATING,
+            'post_id' => 1,
             'book_id' => $faker->randomElement($bookId)
         ]);
-    }
-
-    public function makeQueryDetailPost($id)
-    {
-        $fields = [
-            'posts.status',
-            'ratings.rating',
-            DB::raw('COUNT(likes.id) AS likes'),
-        ];
-
-        $post = Post::select($fields)
-                    ->leftJoin('ratings', 'posts.id', '=', 'ratings.post_id')
-                    ->leftJoin('likes', 'posts.id', '=', 'likes.post_id')
-                    ->groupBy('posts.id', 'ratings.id')
-                    ->findOrFail($id);
-
-        return $post;
     }
 }
