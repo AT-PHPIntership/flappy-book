ed7014b24fcede4ed15a6d1099eede2b50ef6afb
Tram <tram.pham@asiantech.vn>
2018-03-06 10:39:50 +0700

    layout language book

---
+++ resources/lang/en/languages.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'list_languages' => 'List Languages',
+    'home' => 'Home',
+    'languages' => 'Languages',
+    'list' => 'List',
+    'no' => 'No',
+    'name' => 'Name',
+    'options' => 'Options',
+    'add_language' => 'Add Language',
+];
